b781e67fa15735bdbef8b6d30117b4f520da4c07
witrellesp <william.trelles1@eduvaud.ch>
2024-09-27 09:17:48 +0200

    chore(exo maximum): Importer le code de base

---
+++ exos/immutable/maximum/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4 players
+            List<Player> players = new List<Player>()
+            {
+             new Player("Joe", 32),
+            new Player("Jack", 30),
+            new Player("William", 37),
+              new Player("Averell", 25)
+            };
+
+            // Initialize search
+            Player elder = players.First();
+            int biggestAge = elder.Age;
+
+            // search
+            foreach (Player p in players)
+            {
+                if (p.Age > biggestAge) // memorize new elder
+                {
+                    elder = p;
+                    biggestAge = p.Age; // for future loops
+                }
+            }
+
+            Console.WriteLine($"Le plus agé est {elder.Name} qui a {elder.Age} ans");
+
+            Console.ReadKey();
+        }
+
+        public class Player
+        {
+            private readonly string _name;
+            private readonly int _age;
+
+            public Player(string name, int age)
+            {
+                _name = name;
+                _age = age;
+            }
+
+            public string Name => _name;
+
+            public int Age => _age;
+        }
+
+
+    }
+}
+
